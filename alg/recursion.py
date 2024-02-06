778422fcd5bbbffc528690ccc5851ca879246a7e
Мария Фалилеева <falileeva.ma@gmail.com>
2024-02-06 21:56:59 +0800

    version 2

--- alg/recursion.py
+++ alg/recursion.py
@@ -5,13 +5,20 @@
 from typing import List
 
 
-def sum_list(number_list: List[int]) -> int:
+def sum_list_v2(number_list: List[int]) -> int:
+    if len(number_list) == 0:
+        return 0
+    else:
+        return number_list[0] + sum_list_v2(number_list=number_list[1:])
+
+
+def sum_list_v1(number_list: List[int]) -> int:
     result = number_list[0]
 
     if len(number_list) == 1:
         return result
     else:
-        result += sum_list(number_list=number_list[1:])
+        result += sum_list_v1(number_list=number_list[1:])
 
     return result
 
@@ -19,7 +26,11 @@ def sum_list(number_list: List[int]) -> int:
 def tests():
     number_list = [1, 4, 6, 7, 9, 10]
 
-    result = sum_list(number_list=number_list)
+    result = sum_list_v1(number_list=number_list)
+
+    assert result == 37
+
+    result = sum_list_v2(number_list=number_list)
 
     assert result == 37
 
