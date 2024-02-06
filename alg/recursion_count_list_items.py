0c3a3aa55a72c83828eefbc47ea559cccb8f8989
Мария Фалилеева <falileeva.ma@gmail.com>
2024-02-06 22:51:01 +0800

    count_list_items with recursion

---
+++ alg/recursion_count_list_items.py
@@ -0,0 +1,25 @@
+"""
+Подсчет количества элементов в списке с помощью рекурсии
+"""
+
+from typing import List
+
+
+def v(number_list: List[int]) -> int:
+    if number_list == list():
+        return 0
+    else:
+        return 1 + count_list_items(number_list[1:])
+
+
+def tests():
+    number_list = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+    result = count_list_items(number_list=number_list)
+
+    assert result == 9
+
+    print("OK")
+
+
+tests()
