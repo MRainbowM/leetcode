f256c24627229d84bbc548b1abcc42c2db2a26fb
Мария Фалилеева <falileeva.ma@gmail.com>
2024-12-05 17:41:45 +0800

    #1 two version

--- 1_two_sum.py
+++ 1_two_sum.py
@@ -28,11 +28,9 @@ from typing import List
 
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        for i in range(len(nums)):
-            for j in range(len(nums)):
-                if i == j:
-                    continue
-
+        nums_len = len(nums)
+        for i in range(nums_len - 1):
+            for j in range(i + 1, nums_len):
                 if nums[i] + nums[j] == target:
                     return [i, j]
 
